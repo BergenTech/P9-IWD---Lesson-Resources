80734b34a283b5ddded6e0172182e80a87446735
emrgem <emrgem@bergen.org>
2024-04-29 13:57:18 -0400

    updated

--- Unit 5 - JS DOM RESOURCES/lesson 02 - starter code/practices/practice - 1/script.js
+++
@@ -1,7 +0,0 @@
-// Select all <div> elements with class "box" and:
-// change background color to "lightblue"
-
-
-
-// Select <ul> element with ID "list" and 
-//change: text color of all <li> children to "green"
